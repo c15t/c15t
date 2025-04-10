c04b93a346f0a19742086f51b897ccb2e2582782
burnedchris <christopher@everfund.com>
2025-04-10 01:31:31 +0100

    chore: Removed old docs

--- apps/docs/src/app/(home)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import type { ReactNode } from 'react';
-import { homePageOptions } from '~/app/layout.config';
-
-export default function Layout({ children }: { children: ReactNode }) {
-	return <HomeLayout {...homePageOptions}>{children}</HomeLayout>;
-}
