5b142e482bf0fc2c98c6cf22268f6158f23015a0
burnedchris <christopher@everfund.com>
2025-04-19 23:00:28 +0100

    feat: add c15t configuration and enhance global styles
    - Introduced a new example environment file for c15t configuration.
    - Added c15t client configuration to manage consent options.
    - Enhanced global CSS to improve focus styles for buttons and figures, ensuring better accessibility and visual feedback.

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -7,30 +7,82 @@ import {
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
-import { env } from '~/env';
 import { PostHogProvider } from './posthog-provider';
+import { c15tConfig } from 'c15t.config';
+
+// Define theme colors as variables for consistency
 
 const inter = Inter({
 	subsets: ['latin'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const bgColor = '#000';
+  const bgColorDark = '#FFF';
+	const primaryColor = 'hsl(172 72.2% 48%)';
+	const primaryColorHover = 'hsl(172deg 72% 48% / 0.1)';
+  const focusRing = `${primaryColor} !important`;
+  const focusShadow = `0 0 0 2px ${primaryColor}`;
+
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
 			<body className="flex min-h-screen flex-col">
 				<RootProvider>
-					<ConsentManagerProvider
-						options={{
-							mode: 'c15t',
-							backendURL: env.NEXT_PUBLIC_C15T_URL as string,
-							store: {
-								initialGdprTypes: ['necessary', 'marketing'],
-							},
-						}}
-					>
+					<ConsentManagerProvider options={c15tConfig}>
 						<PostHogProvider>{children}</PostHogProvider>
-						<CookieBanner />
-						<ConsentManagerDialog />
+						<CookieBanner
+							theme={{
+								'banner.root': {
+									style: {
+										'--banner-background-color-dark': bgColorDark,
+										'--banner-footer-background-color-dark': bgColorDark,
+										'--button-primary-hover-dark': primaryColorHover,
+                    '--button-primary-hover': primaryColorHover,
+                    '--button-background-color-dark': bgColorDark,
+                    '--button-background-color': bgColor,
+                    '--button-focus-ring-dark': `${primaryColor} !important`,
+										'--button-focus-ring': `${primaryColor} !important`,
+										'--button-primary-dark': primaryColor,
+                    '--button-primary': primaryColor,
+										'--button-shadow-primary-dark': `var(--button-shadow-dark), inset 0 0 0 1px ${primaryColor}`,
+										'--button-shadow-primary-focus-dark': focusShadow,
+                    '--button-shadow-primary-focus': focusShadow,
+										'--button-shadow-primary': `var(--button-shadow), inset 0 0 0 1px ${primaryColor}`,
+									},
+								},
+							}}
+						/>
+						<ConsentManagerDialog
+							theme={{
+								'dialog.root': {
+									style: {
+										'--dialog-background-color-dark': bgColorDark,
+										'--dialog-footer-background-color-dark': bgColorDark,
+										'--button-focus-ring-dark': `${primaryColor} !important`,
+										'--button-focus-ring': `${primaryColor} !important`,
+										'--button-primary-dark': primaryColor,
+                    '--button-primary': primaryColor,
+										'--button-shadow-primary-dark': `var(--button-shadow-dark), inset 0 0 0 1px ${primaryColor}`,
+										'--button-shadow-primary-focus-dark': focusShadow,
+                    '--button-shadow-primary-focus': focusShadow,
+										'--button-shadow-primary': `var(--button-shadow), inset 0 0 0 1px ${primaryColor}`,
+                    '--accordion-focus-ring-dark': focusRing,
+                    '--accordion-focus-ring': focusRing,
+                    '--accordion-focus-shadow-dark': focusShadow, 
+                    '--accordion-focus-shadow': focusShadow,
+                    '--button-primary-hover-dark': primaryColorHover,
+                    '--dialog-branding-focus-color-dark': `var(--button-shadow), inset 0 0 0 1px ${primaryColor}`,
+                    '--switch-focus-shadow-dark': focusShadow,
+                    '--switch-background-color-checked-dark': primaryColor,
+                    '--switch-background-color-checked': primaryColor,
+                    '--switch-background-color-unchecked-dark': bgColorDark,
+                    '--switch-background-color-unchecked': bgColor,
+                    '--widget-accordion-background-color-dark': bgColorDark,
+                    '--widget-accordion-background-color': bgColor,
+									},
+								},
+							}}
+						/>
 					</ConsentManagerProvider>
 				</RootProvider>
 			</body>
