9e8dd8352e38c7358b3ec0e87dc31d502d61ac0f
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2025-09-29 10:03:19 +0100

    fix scripts test

--- packages/scripts/vitest.config.ts
+++ packages/scripts/vitest.config.ts
@@ -1,12 +1,11 @@
 import { resolve } from 'node:path';
 import { baseConfig } from '@c15t/vitest-config/base';
-import react from '@vitejs/plugin-react';
 import { defineConfig, mergeConfig } from 'vitest/config';
 
 export default mergeConfig(
 	baseConfig,
 	defineConfig({
-		plugins: [react()],
+		plugins: [],
 		resolve: {
 			alias: {
 				'~': resolve(__dirname, './src'),
